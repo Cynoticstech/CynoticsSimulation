3b8543768087cad7cbde5f1375dc566416160d75
PAmogh <112743343+PAmogh@users.noreply.github.com>
2023-06-22 21:35:23 +0530

    moobile back btn on redux pages

--- Assets/Akshansh/Scripts/Alpha UI scripts/MobileBackBtn.cs
+++ Assets/Akshansh/Scripts/Alpha UI scripts/MobileBackBtn.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MobileBackBtn : MonoBehaviour
 {
+    [SerializeReference]
+    public GameObject SceneCheckRef;
+    [SerializeReference]
+    public GameObject HomePanel;
+    [SerializeReference]
+    public GameObject BioPanel;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,30 @@ public class MobileBackBtn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if ((SceneCheckRef.scene.name == "Simulations") && Input.GetKeyUp(KeyCode.Escape))
+        {
+            SceneCheckRef.GetComponent<navbtnclk>().alphaload();
+        }
+        if ((SceneCheckRef.scene.name == "Offline_Alpha_Pages") && Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (HomePanel.activeSelf)
+            {
+                SceneCheckRef.GetComponent<navbtnclk>().splashload();
+            }
+            else if (BioPanel.activeSelf)
+            {
+                HomePanel.SetActive(true);
+                BioPanel.SetActive(false);
+            }
+
+        }
+        if ((SceneCheckRef.scene.name == "Splash Screen") && Input.GetKeyUp(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+
     }
+
+    
 }
