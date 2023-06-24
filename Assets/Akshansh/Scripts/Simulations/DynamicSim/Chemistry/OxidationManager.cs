1523b877b851207db54077c529a4792152903bd0
AkshanshKanojia <akshanshkanojia@gmail.com>
2023-06-24 16:13:29 +0530

    Oxidation 1 anim values

--- Assets/Akshansh/Scripts/Simulations/DynamicSim/Chemistry/OxidationManager.cs
+++ Assets/Akshansh/Scripts/Simulations/DynamicSim/Chemistry/OxidationManager.cs
@@ -1,22 +1,29 @@
 using AkshanshKanojia.Inputs.Button;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class OxidationManager : MonoBehaviour
 {
     [SerializeField] DragManager tube, beaker1, beaker2, kmno4;
     [SerializeField]
     float beakerDropSpeed = 1f, beakerRotSpeed = 2f, beakerResetTime = 4f, beakerDropDuration = 4f,
-        tubePosTime = 1f;
-    [SerializeField] int curtLiquidIndex = 0;
+        tubePosTime = 1f, chemDropLiftTime = 1f, chemDropIdleTime = 2f, chemRotTime = 2f, tubeLiftTime = 1f, tubeShakeTime = 0.3f,
+        tubeShakeIdle = 2f;
+    [SerializeField] int curtLiquidIndex = 0, tubeShakeItteration = 3,experimentEndIndex =5;
     [SerializeField] Transform liquidMasks;
     [SerializeField] GameObject flameObj;
     [SerializeField] ButtonInputManager burnerButt;
+    [SerializeField] SpriteRenderer tubeLiquid;
+    [SerializeField] Color chemPink, tubeNormal;
 
     [SerializeField]
     float[] liquidLevels;
-    [SerializeField] Vector3 dropPos, dropRot, tubePos;
+    [SerializeField] Vector3 dropPos, dropRot, tubePos, chemDropPos, chemRotAngle, tubeShakePos, tubeShakeRot;
 
+    public UnityEvent OnSimulationFinished;
+
+    int curtShakeItteration;
     private void Start()
     {
         AssigneEvents();
@@ -26,27 +33,94 @@ public class OxidationManager : MonoBehaviour
     {
         beaker1.OnCorrectPlaced.AddListener(() => OnCorrectBeaker(beaker1));
         beaker2.OnCorrectPlaced.AddListener(() => OnCorrectBeaker(beaker2));
-        tube.OnCorrectPlaced.AddListener(OnTubePlaced);
+        tube.OnCorrectPlaced.AddListener(TubePlaced);
         burnerButt.OnTap += (obj) =>
         {
-            OnBurnerTapped();
+            BurnerTapped();
         };
-        kmno4.OnCorrectPlaced.AddListener(OnChemicalDrop);
+        kmno4.OnCorrectPlaced.AddListener(ChemicalDrop);
     }
-    void OnChemicalDrop()
+    void ChemicalDrop()
     {
         kmno4.DisableInteract();
-        CheckLiquidIndex();
-
+        CheckLiquidIndex();//don't change order
+        DropChem();
     }
-    private void OnBurnerTapped()
+
+    private void DropChem()
+    {
+        var _tempTrans = kmno4.transform;
+        _tempTrans.DOMove(chemDropPos, chemDropLiftTime).onComplete += () =>
+        {
+            _tempTrans.DORotate(chemRotAngle, chemRotTime).SetEase(Ease.Linear).onComplete += () =>
+            {
+                _tempTrans.DORotate(Vector3.zero, chemRotTime).SetEase(Ease.Linear).SetDelay(chemDropIdleTime).onComplete += () =>
+                {
+                    //go back
+                    _tempTrans.DOMove(kmno4.OriginPos, beakerResetTime);
+                    tubeLiquid.DOColor(chemPink, chemDropIdleTime);
+                    //shake tube next
+                    ShakeTube();
+                };
+                //raise chemical level
+                liquidMasks.DOScaleY(liquidLevels[curtLiquidIndex], chemDropIdleTime);
+            };
+        };
+    }
+
+    private void ShakeTube()
+    {
+        var _tubeTrans = tube.transform;
+        _tubeTrans.DOMove(tubeShakePos, tubeLiftTime).onComplete += () =>
+        {
+            RotateTube(_tubeTrans);
+        };
+    }
+
+    private void RotateTube(Transform _tubeTrans)
+    {
+        _tubeTrans.DORotate(tubeShakeRot, tubeShakeTime).SetEase(Ease.Linear).onComplete += () =>
+        {
+            _tubeTrans.DORotate(-tubeShakeRot, tubeShakeTime).SetEase(Ease.Linear).onComplete += () =>
+            {
+                curtShakeItteration++;
+                if (curtShakeItteration >= tubeShakeItteration)
+                {
+                    //reset tube pos
+                    //check Chemical Index
+                    if(curtLiquidIndex <experimentEndIndex)
+                    {
+                        tubeLiquid.DOColor(tubeNormal, tubeShakeIdle);
+                    }
+                    _tubeTrans.DOMove(tubePos, tubeLiftTime).SetDelay(tubeShakeIdle).onComplete+=()=>
+                    {
+                        kmno4.EnableInteract();
+                        if (curtLiquidIndex >= experimentEndIndex)
+                        {
+                            OnSimulationFinished?.Invoke();
+                            kmno4.DisableInteract();
+                            print("Simulation End");
+                        }
+                        //one chem cycle end
+                    };
+                }
+                else
+                {
+                    //shake again
+                    RotateTube(_tubeTrans);
+                }
+            };
+        };
+    }
+
+    private void BurnerTapped()
     {
         burnerButt.enabled = false;
         flameObj.SetActive(true);
         kmno4.EnableInteract();
     }
 
-    void OnTubePlaced()
+    void TubePlaced()
     {
         tube.transform.DOMove(tubePos, tubePosTime).onComplete += () =>
         {
