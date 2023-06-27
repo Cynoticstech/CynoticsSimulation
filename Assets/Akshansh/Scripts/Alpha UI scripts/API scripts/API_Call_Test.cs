eb8a0794333d373cdeade1649a80027e9ee7ea67
PAmogh <112743343+PAmogh@users.noreply.github.com>
2023-06-27 14:38:25 +0530

    api test scripts added

---
+++ Assets/Akshansh/Scripts/Alpha UI scripts/API scripts/API_Call_Test.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class API_Call_Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [System.Obsolete]
+    void Start()
+    {
+        StartCoroutine(SendRequest());
+    }
+    /*private string GetAuthenticationKey()
+    {
+        //throw new NotImplementedException();
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    [System.Obsolete]
+    IEnumerator SendRequest()
+    {
+            string requestURL = "https://cynotics-backend-web-service.onrender.com/api/user/signup";
+            print("Called");
+            data myData = new data()
+            {
+                fullName = "Roy",
+                dob = "11/8/96",
+                email = "Roy@gmail.com",
+                InstituteId = "roy_ID",
+                phone = "1234567890"
+
+            };
+
+            // convert object to JSON
+            string jsonBody = JsonUtility.ToJson(myData);
+
+            // convert JSON to raw bytes
+            byte[] rawBody = Encoding.UTF8.GetBytes(jsonBody);
+
+            UnityWebRequest request = new UnityWebRequest(requestURL, "POST");
+            request.uploadHandler = new UploadHandlerRaw(rawBody);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            //request.SetRequestHeader("Authorization", "Basic " + GetAuthenticationKey());
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            yield return request.SendWebRequest();
+
+            if (request.isNetworkError || request.isHttpError)
+            {
+                // error
+                //request.error
+                Debug.Log(request.error);
+                // request.responseCode
+            }
+
+            else
+            {
+                // successful
+                Debug.Log("Complete");
+            }
+        
+    }
+}
---
+++ Assets/Akshansh/Scripts/Alpha UI scripts/API scripts/LoginAPITestScript.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LoginAPITestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Login()
+    {
+        StartCoroutine(Upload());
+    }
+
+    IEnumerator Upload()
+    {
+
+
+        WWWForm form = new WWWForm();
+        /* string data = "{\"fullName\": \"My test fullName\", \"dob\": \"1682761673617\", " +
+             "\"email\": \"Mytest@test.com\", \"phone\": \"1234567890\",\"InstituteId\": \"Mytest-employee-id\"}";*/
+
+        //Debug.Log(data);
+        data myData = new data();
+        myData.fullName = "MyName";
+        myData.dob = "1025";
+        myData.email = "MyMail.c";
+        myData.phone = "10254785";
+        myData.InstituteId = "EmplyID";
+
+        string dataToSend = JsonUtility.ToJson(myData);
+        
+        form.AddField("myField", "myData");
+        //object jsonObj = JsonUtility.FromJson<object>(data);
+
+        using (UnityWebRequest www = UnityWebRequest.Post
+            ("https://cynotics-backend-web-service.onrender.com/api/user/signup", dataToSend))
+
+        {
+            byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(dataToSend);
+            www.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
+        }
+    }
+}
+class data
+{
+    public string fullName, dob, email, phone, InstituteId;
+}
+class CredentialData
+{
+    public string superAdminAccessKeyc, password;
+}
+
+class DeleteUserClass
+{
+    public string _id;
+}
