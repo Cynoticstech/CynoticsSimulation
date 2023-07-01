2541bc7b8eacef79c32c2b35046f90ed76661f9b
Akash Suyal <3785akash@gmail.com>
2023-07-01 12:17:56 +0530

    pusing

---
+++ Assets/Akash/Scenes/IMEIData.cs
@@ -0,0 +1,27 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+
+#if UNITY_ANDROID
+public static class IMEIData
+{
+    public static string GetIMEI()
+    {
+        string imei = string.Empty;
+
+        using (AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            using (AndroidJavaObject currentActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity"))
+            {
+                string telephonyServiceName = "phone";
+                using (AndroidJavaObject telephonyManager = currentActivity.Call<AndroidJavaObject>("getSystemService", telephonyServiceName))
+                {
+                    imei = telephonyManager.Call<string>("getDeviceId");
+                }
+            }
+        }
+
+        return imei;
+    }
+}
+#endif
