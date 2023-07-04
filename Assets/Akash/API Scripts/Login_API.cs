786c27053c8aaf6aac6def5de8250234dd836e3d
Akash Suyal <3785akash@gmail.com>
2023-07-04 19:26:10 +0530

    Push

--- Assets/Akash/API Scripts/Login_API.cs
+++ Assets/Akash/API Scripts/Login_API.cs
@@ -12,6 +12,8 @@ public class Login_API : MonoBehaviour
     [SerializeField] private TextMeshProUGUI title, message;
     [SerializeField] private GameObject popup;
 
+    public static string userDataJson;
+
     public const string EmailKey = "Email";
     public const string PasswordKey = "Password";
     public const string DeviceKeyKey = "DeviceKey";
@@ -85,7 +87,7 @@ public class Login_API : MonoBehaviour
         if (newRequest.result == UnityWebRequest.Result.Success)
         {
             Debug.Log("Data Retrieved");
-            string userDataJson = newRequest.downloadHandler.text;
+            userDataJson = newRequest.downloadHandler.text;
             SceneManager.LoadScene("Main Alpha Functionality Pages");
         }
         else
