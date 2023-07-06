19c6dfcc51cfe67f5fbea38b661f21112b516b8a
Akash Suyal <3785akash@gmail.com>
2023-07-06 17:54:00 +0530

    Pushed

---
+++ Assets/Akash/API Scripts/ShowPassword.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ShowPassword : MonoBehaviour
+{
+    public TMP_InputField passwordInputField;
+    public Button showPasswordButton;
+
+    private bool isPasswordVisible = false;
+    private TMP_InputField.ContentType originalContentType;
+
+    private void Start()
+    {
+        originalContentType = passwordInputField.contentType;
+        showPasswordButton.onClick.AddListener(TogglePasswordVisibility);
+    }
+
+    public void TogglePasswordVisibility()
+    {
+        isPasswordVisible = !isPasswordVisible;
+        passwordInputField.contentType = isPasswordVisible ? TMP_InputField.ContentType.Standard : TMP_InputField.ContentType.Password;
+        passwordInputField.ForceLabelUpdate();
+        //showPasswordButton.GetComponentInChildren<Text>().text = isPasswordVisible ? "Hide Password" : "Show Password";
+    }
+}
