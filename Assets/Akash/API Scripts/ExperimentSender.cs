c7bd6055160972e933598e47164b43f0017e6e0e
Akash Suyal <3785akash@gmail.com>
2023-07-06 18:55:25 +0530

    Updated

--- Assets/Akash/API Scripts/ExperimentSender.cs
+++ Assets/Akash/API Scripts/ExperimentSender.cs
@@ -19,87 +19,46 @@ public class ExperimentSender : MonoBehaviour
     IEnumerator SendExperimentsData()
     {
         string apiUrl = "https://echo-admin-backend.vercel.app/api/experiments/";
-        List<ExperimentsData> experimentsData = new List<ExperimentsData>();
-        //ExperimentsData expDatum = new ExperimentsData();
 
-
-        ExperimentsData data1 = new ExperimentsData
+        ExperimentsData data = new ExperimentsData
         {
-            experimentName = "Experiment 1",
-            moduleName = "Module A",
+            experimentName = "",
+            moduleName = "",
             user = Get_Student_Details.guid,
             questions = new List<Question>
+        {
+            new Question
             {
-                new Question
+                question = "Vha se htoo ,()",
+                answer = "",
+                attemptedanswer = new List<string>
                 {
-                    question = "Question 1",
-                    answer = "Answer 1",
-                    attemptedanswer = new List<string>
-                    {
-                        "Attempt 1",
-                        "Attempt 2"
-                    }
-                },
-                new Question
-                {
-                    question = "Question 2",
-                    answer = "Answer 2",
-                    attemptedanswer = new List<string>
-                    {
-                        "Attempt 1",
-                        "Attempt 2",
-                        "Attempt 3"
-                    }
+                    "25",
+                    "123"
                 }
             }
-        };
-        experimentsData.Add(data1);
-
-        ExperimentsData data2 = new ExperimentsData
+        },
+            marks = "",
+            comments = new List<Comment>
         {
-            experimentName = "Experiment 2",
-            moduleName = "Module B",
-            user = Get_Student_Details.guid,
-            questions = new List<Question>
+            new Comment
             {
-                new Question
-                {
-                    question = "Question 3",
-                    answer = "Answer 3",
-                    attemptedanswer = new List<string>
-                    {
-                        "Attempt 1"
-                    }
-                },
-                new Question
-                {
-                    question = "Question 4",
-                    answer = "Answer 4",
-                    attemptedanswer = new List<string>
-                    {
-                        "Attempt 1",
-                        "Attempt 2",
-                        "Attempt 3",
-                        "Attempt 4"
-                    }
-                }
-            },
-            
+                name = "John",
+                comments = ""
+            }
+        }
         };
-        experimentsData.Add(data2);
 
-        string jsonData = JsonUtility.ToJson(experimentsData.ToArray());
-        //string jsonData = JsonUtility.ToJson(experimentsData);
-        //string jsonData = JsonUtility.ToJson(data1);
-        /*byte[] rawBody = Encoding.UTF8.GetBytes(jsonData);
+        string jsonData = JsonUtility.ToJson(data);
+
         UnityWebRequest request = UnityWebRequest.Post(apiUrl, "application/json");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonData);
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
         request.SetRequestHeader("Content-Type", "application/json");
-        request.uploadHandler = new UploadHandlerRaw(rawBody);
-        request.downloadHandler = new DownloadHandlerBuffer();*/
-        UnityWebRequest request = UnityWebRequest.Post(apiUrl, jsonData);
-        request.SetRequestHeader("Content-Type", "application/json");
+
         yield return request.SendWebRequest();
-        
+
         if (request.result == UnityWebRequest.Result.Success)
         {
             Debug.Log("Experiments data sent successfully!");
@@ -109,6 +68,7 @@ public class ExperimentSender : MonoBehaviour
             Debug.Log("Failed to send experiments data. Error: " + request.error);
         }
     }
+
 }
 
 [System.Serializable]
