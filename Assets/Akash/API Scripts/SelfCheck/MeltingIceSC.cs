cfeb7fd7d83c786ccd5c722c6be76818325d898c
Akash Suyal <3785akash@gmail.com>
2023-07-14 10:55:07 +0530

    push

--- Assets/Akash/API Scripts/SelfCheck/MeltingIceSC.cs
+++ Assets/Akash/API Scripts/SelfCheck/MeltingIceSC.cs
@@ -35,3 +35,47 @@ public class MeltingIceSC : MonoBehaviour
         }
     }
 }
+
+/*using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MeltingIceSC : MonoBehaviour
+{
+    public string[] texts;
+    public TMP_InputField[] obsFibs;
+    public TMP_InputField[] scFibs;
+
+    void Start()
+    {
+        for (int i = 0; i < obsFibs.Length; i++)
+        {
+            obsFibs[i].text = obsFibs[i].text.ToLower().Replace(" ", "");
+            texts[i] = texts[i].ToLower().Replace(" ", "");
+        }
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void IceSelfCheck()
+    {
+        for (int i = 0; i < obsFibs.Length; i++)
+        {
+            scFibs[i].text = obsFibs[i].text;
+
+            if (obsFibs[i].text == texts[i])
+            {
+                scFibs[i].GetComponent<TMP_Text>().color = Color.green;
+            }
+            else
+            {
+                scFibs[i].GetComponent<TMP_Text>().color = Color.red;
+            }
+        }
+    }
+}*/
+
