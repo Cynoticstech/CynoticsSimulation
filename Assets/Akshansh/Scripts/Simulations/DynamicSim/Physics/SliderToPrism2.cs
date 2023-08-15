a55089d17166afce3b74d8819fa9ff14f0427bd5
PAmogh <112743343+PAmogh@users.noreply.github.com>
2023-08-15 21:00:42 +0530

    prism 4

--- Assets/Akshansh/Scripts/Simulations/DynamicSim/Physics/SliderToPrism2.cs
+++ Assets/Akshansh/Scripts/Simulations/DynamicSim/Physics/SliderToPrism2.cs
@@ -4,19 +4,24 @@ using UnityEngine.UI;
 public class SliderToPrism2 : MonoBehaviour
 {
     public Slider slider;
+    public GameObject incidentRay;
     public GameObject EmergentPoint;
-    
 
     public float minSliderValue = 0.0f;
     public float maxSliderValue = 1.0f;
     public float minRotation = 13.8f;
     public float maxRotation = 35.0f;
 
+    public Vector2 startPos = new Vector2(1.827f, -0.82f);
+    public Vector2 endPos = new Vector2(1.14f, 0.42f);
+
     private void Start()
     {
-        // Set the initial rotation based on minRotation
         float initialRotation = Mathf.Lerp(minRotation, maxRotation, Mathf.InverseLerp(minSliderValue, maxSliderValue, slider.value));
-        //RefractiveRay.transform.localRotation = Quaternion.Euler(0f, 0f, initialRotation);
+        incidentRay.transform.localRotation = Quaternion.Euler(0f, 0f, initialRotation);
+
+        Vector2 initialPosition = Vector2.Lerp(startPos, endPos, Mathf.InverseLerp(minSliderValue, maxSliderValue, slider.value));
+        incidentRay.transform.localPosition = new Vector3(initialPosition.x, initialPosition.y, incidentRay.transform.localPosition.z);
     }
 
     private void Update()
@@ -24,7 +29,9 @@ public class SliderToPrism2 : MonoBehaviour
         float normalizedSliderValue = Mathf.InverseLerp(minSliderValue, maxSliderValue, slider.value);
         float mappedRotation = Mathf.Lerp(minRotation, maxRotation, normalizedSliderValue);
 
-        // Apply the rotation to the GameObject
-        //RefractiveRay.transform.localRotation = Quaternion.Euler(0f, 0f, mappedRotation);
+        incidentRay.transform.localRotation = Quaternion.Euler(0f, 0f, mappedRotation);
+
+        Vector2 newPosition = Vector2.Lerp(startPos, endPos, normalizedSliderValue);
+        incidentRay.transform.localPosition = new Vector3(newPosition.x, newPosition.y, incidentRay.transform.localPosition.z);
     }
 }
