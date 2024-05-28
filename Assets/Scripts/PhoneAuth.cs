c6545b9a827fc98824e4a973581f6b45e4473d7e
Phoenix <shaikhmoinuddin499@gmail.com>
2024-05-28 20:37:04 +0530

    Firebase realtime database is working in simulator but not in real device

--- Assets/Scripts/PhoneAuth.cs
+++ Assets/Scripts/PhoneAuth.cs
@@ -3,37 +3,39 @@ using TMPro;
 using Firebase.Auth;
 using Firebase.Extensions;
 using UnityEngine.SceneManagement;
+using Firebase.Database;
+
+public class User
+{
+    public long PhoneNumber;
+
+    public User(long phoneNumber)
+    {
+        PhoneNumber = phoneNumber;
+    }
+}
 
 public class PhoneAuth : MonoBehaviour
 {
     public TMP_InputField phoneInp, EnterCode_Inp;
-    //public TextMeshProUGUI logTxt;
-    public GameObject AllOptions; //all options
-    //public GameObject SuccessUi;
-    uint phoneAuthTimeoutMs = 3 * 60000; //minutes to milisec
+    public GameObject AllOptions;
+    uint phoneAuthTimeoutMs = 3 * 60000;
     FirebaseAuth auth;
     PhoneAuthProvider provider;
+    DatabaseReference reference;
+    private string UserId;
 
     private void Start()
     {
         auth = FirebaseAuth.DefaultInstance;
         provider = PhoneAuthProvider.GetInstance(auth);
-         // Check if a user is already signed in
-        if (FirebaseAuth.DefaultInstance.CurrentUser != null) {
-            // A user is already signed in
-            Debug.Log("User is already signed in");
-            SceneManager.LoadScene("Main Alpha Functionality Pages");
-        } else {
-            // No user is signed in
-            Debug.Log("No user is signed in");
-            // Show the sign-in UI
-        }
+        UserId = SystemInfo.deviceUniqueIdentifier;
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
     }
 
     public void sendSMS()
     {
         #if UNITY_EDITOR
-        // Use a mock authentication system in the Unity editor
         MockVerifyPhoneNumber();
         #else
         ShowToast("Sending OTP… Please wait");
@@ -52,38 +54,22 @@ public class PhoneAuth : MonoBehaviour
           verificationCompleted: (credential) =>
           {
               print("Verification completed");
-              //showLogMsg("Verification completed");
-
-              // Auto-sms-retrieval or instant validation has succeeded (Android only).
-              // There is no need to input the verification code.
-              // `credential` can be used instead of calling GetCredential().
           },
           verificationFailed: (error) =>
           {
               print("Error: " + error);
-              //showLogMsg(error);
-
-              // The verification code was not sent.
-              // `error` contains a human readable explanation of the problem.
+              
           },
           codeSent: (id, token) =>
           {
-              //showLogMsg("Code send success!!");
-              // Verification code was successfully sent via SMS.
-              // `id` contains the verification id that will need to be passed in with
+              
               PlayerPrefs.SetString("verificationId", id);
-              // the code from the user when calling GetCredential().
-              // `token` can be used if the user requests the code be sent again, to
-              // tie the two requests together.
-
-              // Show toast message
+              
               ShowToast("Verifying OTP… Please wait");
           },
           codeAutoRetrievalTimeOut: (id) =>
           {
-              // Called when the auto-sms-retrieval has timed out, based on the given
-              // timeout parameter.
-              // `id` contains the verification id of the request that timed out.
+              
           });
           #endif
     }
@@ -91,14 +77,14 @@ public class PhoneAuth : MonoBehaviour
     public void VerifyOtp()
     {
         #if UNITY_EDITOR
-        // Use a mock authentication system in the Unity editor
+        
         MockSubmitOtp();
     #else
         print("verify otp pressed");
         string verificationId = PlayerPrefs.GetString("verificationId");
         string verificationCode = EnterCode_Inp.text;
 
-        // Show toast message
+        
         ShowToast("OTP verification in progress…");
 
         PhoneAuthCredential credential = provider.GetCredential(verificationId, verificationCode);
@@ -114,16 +100,20 @@ public class PhoneAuth : MonoBehaviour
 
             FirebaseUser newUser = task.Result.User;
             Debug.Log("User signed in successfully");
-            //showLogMsg("Success");
-            // This should display the phone number.
+            
             Debug.Log("Phone number: " + newUser.PhoneNumber);
-            // The phone number providerID is 'phone'.
+            
             Debug.Log("Phone provider ID: " + newUser.ProviderId);
 
             AllOptions.SetActive(false);
             SceneManager.LoadScene("Main Alpha Functionality Pages");
         });
         #endif
+    }
+    public void CreateUser(){
+        User newUser = new User(long.Parse(phoneInp.text));
+        string json = JsonUtility.ToJson(newUser);
+        reference.Child("users").Child(UserId).SetRawJsonValueAsync(json);
     }
 
     private void ShowToast(string message)
@@ -146,17 +136,16 @@ public class PhoneAuth : MonoBehaviour
     }
     #if UNITY_EDITOR
 private void MockVerifyPhoneNumber() {
-    // Simulate the behavior of Firebase Phone Authentication
+    
     print("Verification completed (mock)");
-    //showLogMsg("Verification completed (mock)");
+    
 }
 #endif
 #if UNITY_EDITOR
 private void MockSubmitOtp() {
-    // Simulate the behavior of Firebase Phone Authentication
+    
     print("OTP submitted (mock)");
-    //showLogMsg("OTP submitted (mock)");
-    // Simulate successful sign-in
+    
     SceneManager.LoadScene("Main Alpha Functionality Pages");
 }
 #endif
