c8216fb75a40439d578a155bb21a741eae9255bf
Phoenix <shaikhmoinuddin499@gmail.com>
2024-06-16 08:54:49 +0530

    Photoelectric experiment integrated

---
+++ Assets/Scripts/PhotoelectricEffectSceneload.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PhotoelectricEffectSceneload : MonoBehaviour
+{
+    // Name of the scene to load
+    private string PhotoelectricEffectSceneName = "PhotoelectricEffect";
+    private string alphaFunctionalityPagesSceneName = "Main Alpha Functionality Pages";
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if the Back button was pressed this frame
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Screen.orientation = ScreenOrientation.Portrait;
+            // If the Back button is pressed, load the Alpha Functionality Pages scene
+            SceneManager.LoadScene(alphaFunctionalityPagesSceneName);
+        }
+    }
+
+    public void OnButtonClick()
+    {
+        // Load the Momentum scene when the button is clicked
+        SceneManager.LoadScene(PhotoelectricEffectSceneName);
+    }
+
+    public void OnButtonClick2()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+        // Load the Momentum scene when the button is clicked
+        SceneManager.LoadScene(alphaFunctionalityPagesSceneName);
+    }
+}
